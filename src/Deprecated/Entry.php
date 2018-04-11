40de97f9cf28a579e6122340b118c4bdc8bbccac
Oliver Earl <ole4@aber.ac.uk>
2018-04-11 05:26:17 +0100

    Removed depreciated code

--- src/Deprecated/Entry.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/bin/Bootstrap.php';
-
-/**
- * This is a temporary workaround until I figure out a more object-oriented way of doing the routing.
- * I mean this is VERY temporary. Because this is ugly. Dogshit ugly.
- * More information inside the /templates/demonstrationrouter.php file.
- */
-require_once __DIR__ . '/templates/DemonstrationRouter.php';
